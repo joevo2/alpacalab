90d000db3741560937eccd143fbc4c19b9b9bf65
Joel MBA <joevo2@gmail.com>
2015-04-05 18:38:06 +0800

    playing with ejs

--- router/main.js
+++ router/main.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
 	app.get('/', function (req, res) {
-		res.render('index.html');
+		res.render('index',{title:"Alpaca Lab"});
 	});
 
 	app.get('/pusheen', function (req, res) {
-		res.render('pusheen.html');
+		res.render('pusheen',{title:"Pusheen :3"});
 	})
 }
