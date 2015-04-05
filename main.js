bbf0c4b28c3ea168005125a5e29fb1f2556aea5b
Joel MBA <joevo2@gmail.com>
2015-04-05 18:28:11 +0800

    clean up file

--- main.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-	app.get('/', function (req, res) {
-		res.render('index.html');
-	});
-
-	app.get('/pusheen', function (req, res) {
-		res.render('pusheen.html');
-	})
-}
