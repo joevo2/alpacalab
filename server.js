603b43729161823abe5066e1c9236cfacdd30ef0
Joel MBA <joevo2@gmail.com>
2015-04-06 22:48:23 +0800

    added braintree server sdk

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express =  require('express');
 var app = express();
+var braintree = require("braintree");
 
 require('./router/main')(app);
 
@@ -12,6 +13,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
+//Braintree SDK
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "3m85hfdt5bgbrwqz",
+  publicKey: "3m85hfdt5bgbrwqz",
+  privateKey: "6jzj2kjy7qspkjr7"
+});
 
 var server = app.listen(app.get('port'), function () {
 	console.log('Express is running on port 5000');
