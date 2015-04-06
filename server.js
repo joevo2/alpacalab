b176c8348fe990c63e69537021774f378f35b61c
Joel MBA <joevo2@gmail.com>
2015-04-06 12:14:58 +0800

    potential fix for heroku deployment

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var app = express();
 
 require('./router/main')(app);
 
+app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function () {
-	console.log('Express is running on port 3000');
+var server = app.listen(app.get('port'), function () {
+	console.log('Express is running on port 5000');
 })
