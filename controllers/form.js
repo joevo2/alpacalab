7b4e47939f4613acd01cf6f14fcb8032c30ec96d
Joel MBA <joevo2@gmail.com>
2015-04-17 16:42:35 +0800

    Implemented basic form handling to database, need to implement error handling

--- controllers/form.js
+++ controllers/form.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 	var bodyParser = require('body-parser')
 	//PG stuff
 	var pg = require('pg');
-	var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+	var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pusheen';
 
 	
 	// create application/x-www-form-urlencoded parser
@@ -10,32 +10,23 @@ module.exports = function(app) {
 
 	//Post request
 	app.post('/pusheen', urlencodedParser, function (req, res) {
-		console.log('Email: ' + req.body.email);
-
-		var results = [];
+		console.log("Name: "+req.body.name);
+		console.log("Email: "+req.body.email);
+		console.log("phone: "+req.body.phone);
+		console.log("address: "+req.body.address);
 
 	    // Grab data from http request
-	    var data = {text: req.body.email, complete: false};
+	    var data = {name: req.body.name, email: req.body.email, phone: req.body.phone, address: req.body.address, paid: false};
 
 	    // Get a Postgres client from the connection pool
 	    pg.connect(connectionString, function(err, client, done) {
 	        // SQL Query > Insert Data
-	        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
-	        // SQL Query > Select Data
-	        var query = client.query("SELECT * FROM items ORDER BY id ASC");
-	        // Stream results back one row at a time
-	        query.on('row', function(row) {
-	            results.push(row);
-	        });
-	        // After all data is returned, close connection and return results
-	        query.on('end', function() {
-	            client.end();
-	            return res.json(results);
-	        });
+	        client.query("INSERT INTO orders(name, email, phone, address, paid) values($1, $2, $3, $4, $5)", [data.name, data.email, data.phone, data.address, data.paid]);
 	        // Handle Errors
-	        if(err) {
-	          console.log(err);
-	        }
+	        if (err)
+		    	{ console.log(err); res.send("Error " + err); }
+		    // After all data is returned, close connection and return results
+            return res.render('pusheen',{title:"Pusheen :3"});
 	    });
 	});
 }
--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pusheen';
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+var query = client.query('CREATE TABLE orders (id SERIAL PRIMARY KEY, name VARCHAR(40) not null, email VARCHAR(40) not null,phone VARCHAR(20) not null,address VARCHAR(100) not null,paid BOOLEAN)');
 query.on('end', function() { client.end(); });
