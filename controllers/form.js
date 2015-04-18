f28a8483d4b9ba39a49bc346cbc6b52e2cfc8608
Joel MBA <joevo2@gmail.com>
2015-04-18 10:34:36 +0800

    fixing heroku error

--- controllers/form.js
+++ controllers/form.js
@@ -14,16 +14,20 @@ module.exports = function(app) {
 		console.log("Email: "+req.body.email);
 		console.log("phone: "+req.body.phone);
 		console.log("address: "+req.body.address);
-		if (req.body.name != null && req.body.email != null && req.body.phone != null && req.body.address != null) {};
-	    // Grab data from http request
-	    var data = {name: req.body.name, email: req.body.email, phone: req.body.phone, address: req.body.address, paid: false};
+		if (req.body.name != null && req.body.email != null && req.body.phone != null && req.body.address != null) {
+			// Grab data from http request
+		    var data = {name: req.body.name, email: req.body.email, phone: req.body.phone, address: req.body.address, paid: false};
 
-	    // Get a Postgres client from the connection pool
-	    pg.connect(connectionString, function(err, client, done) {
-	        // SQL Query > Insert Data
-	        client.query("INSERT INTO orders(name, email, phone, address, paid) values($1, $2, $3, $4, $5)", [data.name, data.email, data.phone, data.address, data.paid]);
-	        if (err) { console.log(err); res.send("Error " + err); }
-            return res.render('pusheen',{title:"Pusheen :3"});
-	    });
+		    // Get a Postgres client from the connection pool
+		    pg.connect(connectionString, function(err, client, done) {
+		        // SQL Query > Insert Data
+		        client.query("INSERT INTO orders(name, email, phone, address, paid) values($1, $2, $3, $4, $5)", [data.name, data.email, data.phone, data.address, data.paid]);
+		        if (err) { console.log(err); res.send("Error " + err); }
+	            return res.render('pusheen',{title:"Pusheen :3"});
+		    });
+		};
+	    else {
+	    	
+	    }
 	});
 }
