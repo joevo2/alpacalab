519a22adc4b962c797f8235fcd5d514aa23552b3
Joel MBA <joevo2@gmail.com>
2015-04-19 22:39:06 +0800

    re-indent whole thing and added admin panel

--- router/main.js
+++ router/main.js
@@ -10,4 +10,8 @@ module.exports = function(app) {
 	app.get('/login', function (req, res) {
 		res.render('login',{title:"Login"});
 	})
+
+	app.get('/admin', function (req, res) {
+		res.render('admin',{title:"Admin Panel"});
+	})
 }
